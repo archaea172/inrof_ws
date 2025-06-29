1116a50cb05462df0a282334018b2e8b1333a77e
aky <akiyugata271@gmail.com>
2025-06-30 03:18:28 +0900

    ros2のノードを立ち上げ

--- Assets/scripts/status_control.cs
+++ Assets/scripts/status_control.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class status_control : MonoBehaviour
+namespace ROS2
 {
-    // Start is called before the first frame update
-    void Start()
+    public class status_control : MonoBehaviour
     {
-        
-    }
+        private ROS2UnityComponent ros2Unity;
+        private ROS2Node ros2Node;
+        // Start is called before the first frame update
+        void Start()
+        {
+            ros2Unity = GetComponent<ROS2UnityComponent>();
+            if (ros2Unity.Ok())
+            {
+                if (ros2Node == null)
+                {
+                    ros2Node = ros2Unity.CreateNode("status_controler");
+                }
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
