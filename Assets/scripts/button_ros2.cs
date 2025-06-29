69e3048d0d209242f98702c40f0d8cf780fc0f0a
aky <akiyugata271@gmail.com>
2025-06-30 02:14:57 +0900

    書いてみた
    ボタン追加

--- Assets/scripts/button_ros2.cs
+++ Assets/scripts/button_ros2.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class button_ros2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace ROS2
+{
+    public class button_ros2 : MonoBehaviour
     {
-        
+        private ROS2UnityComponent ros2Unity;
+        private ROS2Node ros2Node;
+        private IPublisher<std_msgs.msg.String> string_pub;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ros2Unity = GetComponent<ROS2UnityComponent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void on_click()
+        {
+            if (ros2Unity.Ok())
+            {
+                if (ros2Node == null)
+                {
+                    ros2Node = ros2Unity.CreateNode("unity_ros2_node");
+                    string_pub = ros2Node.CreatePublisher<std_msgs.msg.String>("button");
+                }
+                std_msgs.msg.String txdata = new std_msgs.msg.String();
+                txdata.Data = "hello world from button";
+                string_pub.Publish(txdata);
+            }
+        }
     }
 }
